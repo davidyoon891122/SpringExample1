d670b1fd80847040a47ae8e51b6751ad24534d17
David Yoon <qqpo12@naver.com>
2025-02-10 23:31:46 +0900

    Create an UserDao
    * Created an UserDao to add and get user info from the database

---
+++ src/main/java/com/example/springExample1/SpringExample1Application.java
@@ -0,0 +1,35 @@
+package com.example.springExample1;
+
+import com.example.springExample1.user.dao.UserDao;
+import com.example.springExample1.user.domain.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class SpringExample1Application {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		SpringApplication.run(SpringExample1Application.class, args);
+
+		UserDao userDao = new UserDao();
+
+		User user = new User();
+		user.setId("davidyoon");
+		user.setName("davidyoon");
+		user.setPassword("1234");
+
+		userDao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User resultUser = userDao.get("davidyoon");
+		System.out.println(resultUser.getName());
+		System.out.println(resultUser.getPassword());
+
+		System.out.println(resultUser.getId() + " 조회 성공");
+
+	}
+
+}
